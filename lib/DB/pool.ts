6638037bd4c0950bfaabf463a17b43fa2a3fce9a
MD Shariful Islam <masom3456@gmail.com>
2024-10-29 21:39:50 +0600

    add

--- lib/DB/pool.ts
+++ lib/DB/pool.ts
@@ -9,8 +9,9 @@ export  const pool = mysql.createPool({
 })
 
 export  const DbPool = mysql.createPool({
-    host:process.env.MYSQL_HOST,
-    password:'',
-    user:'admin',
-    database:'user_database'
+    host:'mysql.railway.internal',
+    password:'wbYCBMWhMnnaigAbnORiNuFpultabNeM',
+    user:'root',
+    database:'railway',
+    port:3306
 })

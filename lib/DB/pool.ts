59c6f3af8d1dd45019ab3000735bdf62650caabb
MD Shariful Islam <masom3456@gmail.com>
2024-10-30 07:37:46 +0600

    try

--- lib/DB/pool.ts
+++ lib/DB/pool.ts
@@ -1,12 +1,6 @@
 import mysql from "mysql2/promise"
 
-export  const pool = mysql.createPool({
-    host:'mysql.railway.internal',
-    password:'wbYCBMWhMnnaigAbnORiNuFpultabNeM',
-    user:'root',
-    database:'railway',
-    port:3306
-})
+export  const pool = mysql.createPool('mysql://root:wbYCBMWhMnnaigAbnORiNuFpultabNeM@mysql.railway.internal:3306/railway')
 
 export  const DbPool = mysql.createPool({
     host:'mysql.railway.internal',

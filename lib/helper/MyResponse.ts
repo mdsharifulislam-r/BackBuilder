181aac97141dfc658f5faa65587abb5ce7bd9484
MD Shariful Islam <masom3456@gmail.com>
2024-11-02 13:18:03 +0600

    some

--- lib/helper/MyResponse.ts
+++ lib/helper/MyResponse.ts
@@ -7,5 +7,6 @@ export const MyResponse = (body:any,status?:number)=>new Response(body,{
         'Access-Control-Allow-Origin': "*",
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Content-Type': 'application/json',
       },
 })

1a6a6a16675ac44817299134cea198368563fea8
MD Shariful Islam <masom3456@gmail.com>
2024-11-02 13:06:13 +0600

    latest

--- lib/helper/MyResponse.ts
+++ lib/helper/MyResponse.ts
@@ -1,9 +1,10 @@
+import { Number } from "mongoose";
 import { NextResponse } from "next/server";
 
-export const MyResponse = (body:any,status:number,origins?:string[])=>NextResponse.json(body,{
+export const MyResponse = (body:any,status?:number)=>new Response(body,{
     status:status,
     headers: {
-        'Access-Control-Allow-Origin': origins?.length?origins.toString():"*",
+        'Access-Control-Allow-Origin': "*",
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },

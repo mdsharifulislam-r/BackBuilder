4f93102e5a4e4f92321d3eebfe375b886d5ca907
MD Shariful Islam <masom3456@gmail.com>
2024-11-02 13:28:10 +0600

    m

--- lib/helper/MyResponse.ts
+++ lib/helper/MyResponse.ts
@@ -1,12 +1,12 @@
 import { Number } from "mongoose";
 import { NextResponse } from "next/server";
 
-export const MyResponse = (body:any,status?:number)=>new Response(body,{
-    status:status,
-    headers: {
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Content-Type': 'application/json',
-      },
+export const MyResponse = (body:any,status?:number)=>new Response(JSON.stringify(body),{
+  status:status,
+  headers: {
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Content-Type': 'application/json',
+    },
 })
